0d2c5faa63399f1176c09dbd37689fbe27b89d5b
stineb <benjamin.stocker@gmail.com>
2015-01-17 23:58:35 +0000

    work in progress

--- tools_sofun/plot_seasonal.R
+++ tools_sofun/plot_seasonal.R
@@ -1,35 +1,58 @@
 runname <- "RUNNAME"
 outdir <- "/alphadata01/bstocker/sofun/trunk/output/"
-vars <- c("cex","cleaf","gpp","netmin","ninorg","npp","nup","nfixfree")
+dvars <- c("cex","cleaf","gpp","netmin","ninorg","npp","nup","nfixfree")
+avars <- c("calloc")
 
 plotyear <- 2000
 
 nmonths <- 12
 ndayyear <- 365
 ndaymonth <- c(31,28,31,30,31,30,31,31,30,31,30,31)
-nvars <- length(vars)
+ndvars <- length(dvars)
+navars <- length(avars)
+
+##--------------------------------------
+## construct data frame for daily values
+##--------------------------------------
 
 ## get time data
-tmp <- read.table( paste(outdir,runname,".d.",vars[1],".dat",sep=""))
+tmp <- read.table( paste(outdir,runname,".d.",dvars[1],".dat",sep=""))
 time <- tmp$V1
 nyears <- length(time)/ndayyear
 
-## construct data frame
-df <- data.frame( time = time )
-for (ivar in seq(nvars)) {
-  df[[ vars[ivar] ]] <- read.table( paste(outdir,runname,".d.",vars[ivar],".dat",sep=""))[,2]
+daily <- data.frame( time = time )
+for (ivar in seq(ndvars)) {
+  daily[[ dvars[ivar] ]] <- read.table( paste(outdir,runname,".d.",dvars[ivar],".dat",sep=""))[,2]
 }
-df[[ "year" ]] <- as.integer(time)
-df[[ "moy" ]]  <- rep(rep(seq(12),times=ndaymonth),nyears)
-df[[ "doy" ]]  <- rep(seq(ndayyear),nyears)
+daily[[ "year" ]] <- as.integer(time)
+daily[[ "moy" ]]  <- rep(rep(seq(12),times=ndaymonth),nyears)
+daily[[ "doy" ]]  <- rep(seq(ndayyear),nyears)
 
 ## take subset of one year and normalise all variables to [0,1]
-df_sub <- df[df$year==plotyear,]
-for (ivar in seq(nvars)) {
-  df_sub[[ paste(vars[ivar],".norm", sep="") ]] <- df_sub[[ vars[ivar] ]] / max( df_sub[[ vars[ivar] ]], na.rm=TRUE )
+daily_sub <- daily[daily$year==plotyear,]
+for (ivar in seq(ndvars)) {
+  daily_sub[[ paste(dvars[ivar],".norm", sep="") ]] <- daily_sub[[ dvars[ivar] ]] / max( daily_sub[[ dvars[ivar] ]], na.rm=TRUE )
 }
 
+##--------------------------------------
+## construct data frame for annual values
+##--------------------------------------
+## get time data
+tmp <- read.table( paste(outdir,runname,".a.",avars[1],".dat",sep=""))
+time <- tmp$V1
+nyears <- length(time)
+
+annual <- data.frame( time = time )
+for (ivar in seq(navars)) {
+  annual[[ avars[ivar] ]] <- read.table( paste(outdir,runname,".a.",avars[ivar],".dat",sep=""))[,2]
+}
+annual[[ "year" ]] <- as.integer(time)
+
+annual_sub <- annual[which(annual$year==plotyear),]
+
+##--------------------------------------
 ## Plot absolute C variables
+##--------------------------------------
 aspect <- 0.5
 magn <- 4
 ncols <- 2
@@ -49,21 +72,23 @@ panel <- layout(
 # layout.show(panel)
 
 par(mar=c(4,4,0,1))
-plot( df_sub$doy, df_sub$gpp, type="l", xlab="", ylab="gC/m2/d", ylim=c(0,max(df_sub$gpp)) )
-lines( df_sub$doy, df_sub$npp, type="l", xlab="day of year", ylab="gC/m2/d", col="green" )
-lines( df_sub$doy, df_sub$cex, type="l", xlab="day of year", ylab="gC/m2/d", col="blue" )
+plot( daily_sub$doy, daily_sub$gpp, type="l", xlab="", ylab="gC/m2/d", ylim=c(0,max(daily_sub$gpp)) )
+lines( daily_sub$doy, daily_sub$npp, type="l", xlab="day of year", ylab="gC/m2/d", col="green" )
+lines( daily_sub$doy, daily_sub$cex, type="l", xlab="day of year", ylab="gC/m2/d", col="blue" )
 legend( "bottomleft", c("GPP","NPP","EXU"), col=c("black","green","blue"), bty="n", lty=1 )
 
 par(mar=c(0,0,0,0))
 plot( c(0,1), c(0,1), type="n", axes=FALSE )
 text( 0, 0.98, "ANNUAL TOTALS", adj=c(0,0),font=2)
-text( 0, 0.93, paste("NPP",as.character(format(sum(df_sub$npp),digits=2))) , adj=c(0,0))
-text( 0, 0.88, paste("CEX ",as.character(format(sum(df_sub$cex),digits=2))," (",as.character(format(sum(df_sub$cex)/sum(df_sub$npp)*100,digits=2)),"% of NPP)",sep=""), adj=c(0,0))
+text( 0, 0.93, paste("GPP",as.character(format(sum(daily_sub$gpp),digits=2))) , adj=c(0,0))
+text( 0, 0.88, paste("NPP",as.character(format(sum(daily_sub$npp),digits=2))) , adj=c(0,0))
+text( 0, 0.83, paste("CEX ",as.character(format(sum(daily_sub$cex),digits=2))," (",as.character(format(sum(daily_sub$cex)/sum(daily_sub$npp)*100,digits=2)),"% of NPP)",sep=""), adj=c(0,0))
+text( 0, 0.78, paste("C allocated ",as.character(format(annual_sub$calloc,digits=2))), adj=c(0,0))
 
 
 par(mar=c(4,4,0,1))
-plot( df_sub$doy, df_sub$ninorg, type="l", xlab="day of year", ylab="gN/m2/d", ylim=c(0,max(df_sub$ninorg)) )
-lines( df_sub$doy, df_sub$nup, type="l", xlab="day of year", ylab="gC/m2/d", col="green" )
-lines( df_sub$doy, df_sub$netmin, type="l", xlab="day of year", ylab="gC/m2/d", col="blue" )
-lines( df_sub$doy, df_sub$nfixfree, type="l", xlab="day of year", ylab="gC/m2/d", col="red" )
+plot( daily_sub$doy, daily_sub$ninorg, type="l", xlab="day of year", ylab="gN/m2/d", ylim=c(0,max(daily_sub$ninorg)) )
+lines( daily_sub$doy, daily_sub$nup, type="l", xlab="day of year", ylab="gC/m2/d", col="green" )
+lines( daily_sub$doy, daily_sub$netmin, type="l", xlab="day of year", ylab="gC/m2/d", col="blue" )
+lines( daily_sub$doy, daily_sub$nfixfree, type="l", xlab="day of year", ylab="gC/m2/d", col="red" )
 legend( "bottomleft", c("Ninorg","Nup","net N min.","free-living BNF"), col=c("black","green","blue","red"), bty="n", lty=1 )
