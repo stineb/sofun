6517a64a6746e550bd5f2d2a19f61c013d2f8782
stineb <benjamin.stocker@gmail.com>
2015-05-21 17:12:11 +0200

    corrected position of output collection in littersom SR

--- tools_sofun/plot_seasonal.R
+++ tools_sofun/plot_seasonal.R
@@ -1,7 +1,7 @@
 # runname <- "CH-Oe1_2002"
-runname <- "test_pmodel"
+runname <- "test_lastworking"
 outdir <- "/alphadata01/bstocker/sofun/trunk/output/"
-dvars <- c("gpp","npp","cex","nup","nup_pas","nup_act","nup_fix","nup_ret","nfixfree","cleaf","netmin","ninorg","ccost")
+dvars <- c("gpp","npp","cex","nup","nup_pas","nup_act","nup_fix","nup_ret","nfixfree","cleaf","netmin","ninorg","ccost","cleaf","croot","clabl","clitt")
 avars <- c("calloc","nalloc","clit2soil","nlit2soil","nreq","cveg2lit","nveg2lit")
 
 plotyear <- 2000
@@ -23,6 +23,7 @@ time <- tmp$V1
 nyears <- length(time)/ndayyear
 
 daily <- data.frame( time = time )
+print("reading daily output files ...")
 for (ivar in seq(ndvars)) {
   filn <- paste(outdir,runname,".d.",dvars[ivar],".out",sep="")
   print(filn)
@@ -50,8 +51,11 @@ time <- tmp$V1
 nyears <- length(time)
 
 annual <- data.frame( time = time )
+print("reading annual output files ...")
 for (ivar in seq(navars)) {
-  annual[[ avars[ivar] ]] <- read.table( paste(outdir,runname,".a.",avars[ivar],".out",sep=""))[,2]
+  filn <- paste(outdir,runname,".a.",avars[ivar],".out",sep="")
+  print(filn)
+  annual[[ avars[ivar] ]] <- read.table( filn )[,2]
 }
 annual[[ "year" ]] <- as.integer(time)
 
@@ -76,7 +80,7 @@ heights <- rep(aspect*magn,nrows)
 ##--------------------------------------
 ## C FLUXES
 ##--------------------------------------
-pdf(paste("Coverview_sofun_seasonal.pdf"),width=sum(widths),height=sum(heights))
+pdf(paste("C_flux_overview_sofun_seasonal.pdf"),width=sum(widths),height=sum(heights))
 panel <- layout(
                 matrix(c(1:(nrows*ncols)),nrows,ncols,byrow=TRUE),
                 widths=widths,
@@ -108,7 +112,7 @@ dev.off()
 ##--------------------------------------
 ## N FLUXES
 ##--------------------------------------
-pdf(paste("Noverview_sofun_seasonal.pdf"),width=sum(widths),height=sum(heights))
+pdf(paste("N_flux_overview_sofun_seasonal.pdf"),width=sum(widths),height=sum(heights))
 panel <- layout(
                 matrix(c(1:(nrows*ncols)),nrows,ncols,byrow=TRUE),
                 widths=widths,
@@ -165,7 +169,7 @@ dev.off()
 ##--------------------------------------
 ## CUMULATIVE N FLUXES
 ##--------------------------------------
-pdf(paste("Noverview_cumulative_sofun_seasonal.pdf"),width=sum(widths),height=sum(heights))
+pdf(paste("N_flux_overview_cumulative_sofun_seasonal.pdf"),width=sum(widths),height=sum(heights))
 panel <- layout(
                 matrix(c(1:(nrows*ncols)),nrows,ncols,byrow=TRUE),
                 widths=widths,
@@ -198,10 +202,28 @@ text( 0, 0.48, "N fBNF", adj=c(0,0));            text( 0.5, 0.48, as.character(f
 text( 0, 0.43, "N net min.", adj=c(0,0));        text( 0.5, 0.43, as.character(formatC(sum(daily_sub$netmin),digits=2,format="f")) , adj=c(1,0))
 dev.off()
 
+# ##--------------------------------------
+# ## GPP model - obs.
+# ##--------------------------------------
+# pdf(paste("gpp_model_obs_seasonal.pdf"),width=sum(widths),height=sum(heights))
+# panel <- layout(
+#                 matrix(c(1:(nrows*ncols)),nrows,ncols,byrow=TRUE),
+#                 widths=widths,
+#                 heights=heights,
+#                 TRUE
+#                 )
+# # layout.show(panel)
+
+# par(mar=c(4,4,2,1), las=1)
+# plot( daily_sub$doy, daily_sub$gpp, type="l", xlab="", ylab="gC/m2/d", ylim=c(0,max(daily_sub$gpp)) )
+# legend( "bottomleft", c("simulated GPP"), col=c("black"), bty="n", lty=1 )
+
+# dev.off()
+
 ##--------------------------------------
-## GPP model - obs.
+## C POOLS
 ##--------------------------------------
-pdf(paste("gpp_model_obs_seasonal.pdf"),width=sum(widths),height=sum(heights))
+pdf(paste("C_pool_overview_sofun_seasonal.pdf"),width=sum(widths),height=sum(heights))
 panel <- layout(
                 matrix(c(1:(nrows*ncols)),nrows,ncols,byrow=TRUE),
                 widths=widths,
@@ -211,8 +233,21 @@ panel <- layout(
 # layout.show(panel)
 
 par(mar=c(4,4,2,1), las=1)
-plot( daily_sub$doy, daily_sub$gpp, type="l", xlab="", ylab="gC/m2/d", ylim=c(0,max(daily_sub$gpp)) )
-legend( "bottomleft", c("simulated GPP"), col=c("black"), bty="n", lty=1 )
+plot( daily_sub$doy, daily_sub$cleaf, type="l", xlab="", ylab="gC/m2/d", ylim=c(0,max(daily_sub$cleaf)) )
+lines( daily_sub$doy, daily_sub$croot, type="l", xlab="day of year", ylab="gC/m2/d", col="green" )
+lines( daily_sub$doy, daily_sub$clabl, type="l", xlab="day of year", ylab="gC/m2/d", col="blue" )
+legend( "bottomleft", c("leaf C","root C","labile C"), col=c("black","green","blue"), bty="n", lty=1 )
+
+# par(mar=c(0,0,2,0))
+# plot( c(0,1), c(0,1), type="n", axes=FALSE )
+# lab <- expression(paste("(gC m"^-2," yr"^-1,")", sep=""))
+# text( 0, 0.98, "ANNUAL TOTALS", adj=c(0,0),font=2); text( 0.5, 0.98, lab, adj=c(0,0),font=2, cex=0.7 )
+# text( 0, 0.93, "GPP", adj=c(0,0));          text( 0.5, 0.93, as.character(formatC(sum(daily_sub$gpp),digits=1,format="f")) , adj=c(1,0))
+# text( 0, 0.88, "NPP", adj=c(0,0));          text( 0.5, 0.88, as.character(formatC(sum(daily_sub$npp),digits=1,format="f")) , adj=c(1,0))
+# text( 0, 0.83, "CEX ", adj=c(0,0));         text( 0.5, 0.83, as.character(formatC(sum(daily_sub$cex),digits=1,format="f")) , adj=c(1,0)); text( 0.55, 0.83, paste( as.character(formatC(sum(daily_sub$cex)/sum(daily_sub$npp)*100,digits=1,format="f")),"% of NPP",sep=""), adj=c(0,0))
+# text( 0, 0.78, "C -> veg ", adj=c(0,0));    text( 0.5, 0.78, as.character(formatC(annual_sub$calloc,digits=1,format="f")), adj=c(1,0))
+# text( 0, 0.73, "C veg -> lit", adj=c(0,0)); text( 0.5, 0.73, as.character(formatC(annual_sub$cveg2lit,digits=1,format="f")), adj=c(1,0))
+# text( 0, 0.68, "C lit -> soil", adj=c(0,0));text( 0.5, 0.68, as.character(formatC(annual_sub$clit2soil,digits=1,format="f")), adj=c(1,0)); text( 0.55, 0.68, paste(as.character(formatC(annual_sub$eff*100,digits=1,format="f")),"% efficiency",sep=""), adj=c(0,0))
 
 dev.off()
 
