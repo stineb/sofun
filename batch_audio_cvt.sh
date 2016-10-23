819cbbe3d48c5f02bb3ba4f1d53377b600a9932b
twdavis <twd34@cornell.edu>
2016-10-23 16:33:58 -0400

    fixed batch audio converter

--- batch_audio_cvt.sh
+++ batch_audio_cvt.sh
@@ -5,38 +5,37 @@
 # written by Tyler W. Davis
 #
 # 2015-07-04 -- created
-# 2015-07-04 -- last updated
+# 2016-10-23 -- last updated
 #
 # ~~~~~~~~~~~~
 # description:
 # ~~~~~~~~~~~~
-# This script performs batch audio/video conversion based on VLC Media Player
-# (https://www.videolan.org/vlc/). For example, to batch convert lossless m4a 
-# audio to lossly mp3: `bash batch_audio_cvt.sh *.m4a`
+# This script performs batch audio conversion from m4a to mp3.
+#
+# ~~~~~~~~~~~~~
+# requirements:
+# ~~~~~~~~~~~~~
+# libav-tools (e.g., v.6:9.18-0ubuntu0.14.04)
+# mediainfo (e.g, v.0.7.67)
 #
 # ~~~~~~~~~~~
 # references:
 # ~~~~~~~~~~~
-# "How to Batch Encode," online: https://wiki.videolan.org/How_to_Batch_Encode
+# http://giantdorks.org/alain/a-few-methods-to-convert-aac-m4a-to-mp3-under-linux/
 #
-mux="mp3"
-vlc="/usr/bin/vlc"
+# ~~~~~~
+# usage:
+# ~~~~~~
+# `bash batch_audio_cvt.sh *.m4a`
 
-if [ ! -e "$vlc" ]; then
-    echo "Command '$vlc' does not exist"
-    exit 1
-fi
+GetInfo()
+{
+  info=$(mediainfo "$f" | sed -r 's/[ ]+:[ ]/|/') 
+  bitrate=$(echo "$info" | awk -F'|' '$1=="Bit rate" {gsub(/([ A-Za-z]+)/,"",$2); print$2}')
+}
 
-for file in "$@"; do
-    echo "=> Transcoding '$file'... "
-
-    # NOTE: basename is setup for letter-number-letter format (e.g., m4a)
-    dst=`dirname "$file"`
-    new=`basename "$file" | sed 's@\.[a-z][0-9][a-z]$@@'`.$mux
-
-    $vlc -I dummy -q "$file" \
-       --sout "#transcode{acodec=mp4a,ab=128}:standard{mux=mp4,dst=\"$dst/$new\",access=file}" \
-       vlc://quit
-    ls -lh "$file" "$dst/$new"
-    echo
+for f in "$@"; do
+  GetInfo
+  avconv -i "$f" -ab ${bitrate}k "${f%.m4a}.mp3"
 done
+
