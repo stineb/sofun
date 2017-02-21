91ab97cb44ef1085d3cd582939ae7242ca382894
stineb <benjamin.stocker@gmail.com>
2017-02-21 09:13:42 +0100

    changed scripts for getmodissubsets so that they can be run on CX1

---
+++ submit_getmodissubsets_XXXXXX.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#PBS -l walltime=01:00:00,mem=1gb
+## This tells the batch manager to limit the walltime for the job to XX hours, YY minutes and ZZ second
+## and use PP gb of memory.
+
+module load R
+module unload liblzma
+
+## This jobs requires the Intel math kernel so we must load it at run time.
+
+R CMD BATCH --no-save --no-restore '--args sitename="XXXXX"' $HOME/sofun/getin/get_modissubset_tseries.R
+
+## This tells the batch manager to execute the program lazy from the examples
+## directory of the users home directory.
