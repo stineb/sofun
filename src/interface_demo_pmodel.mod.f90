928089b33856dc9b9891ab99862bcc5fcd65ecaa
Benjamin Stocker <benjamin.stocker@gmail.com>
2019-12-02 19:35:56 +0100

    bugfix for demo_pmodel

--- src/interface_demo_pmodel.mod.f90
+++ src/interface_demo_pmodel.mod.f90
@@ -20,6 +20,7 @@ module md_interface
     logical :: is_calib             ! whether this simulation is a calibration simulation (overriding parameters and no output)
 
     logical :: soilmstress          ! when true, an empirical soil moisture stress function is applied to GPP
+    logical :: tempstress           ! when true, an empirical temperature stress function is applied to GPP
     
     character(len=256) :: runname
     character(len=256) :: fapar_forcing_source
