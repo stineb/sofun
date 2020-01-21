0b032c4c78c37b7a4a2556782fa8c9d92abe224d
Benjamin Stocker <benjamin.stocker@gmail.com>
2020-01-21 13:09:32 +0100

    resolved bug with equi_days and idays

--- utils/check_outputs_lm3ppa.R
+++ utils/check_outputs_lm3ppa.R
@@ -21,3 +21,50 @@ ggplot() +
   geom_point(aes(x = LAI_orig, y = LAI_sofun), data = df_test) +
   geom_abline(intercept=0, slope=1, linetype="dotted")
   
+
+
+
+#########3
+
+
+library(ggplot2)
+library(readr)
+
+# Files: Ecosystem_yearlytest.csv; Ecosystem_dailytest.csv; Annual_cohortstest.csv, Cohorts_dailytest.csv; PhotosynthesisDynamicstest.csv
+outBiomeE <- read_csv("~/cagibi/BiomeE-Allocation/model/output/Ecosystem_yearlytest.csv")
+str(outBiomeE)
+dim(outBiomeE)
+
+outSOFUN <- read_csv("/Users/bestocke/sofun/output/Ecosystem_yearly_test.csv")
+str(outSOFUN)
+dim(outSOFUN)
+
+# Subset data Ecosystem_daily_test in outputs_SOFUN from year = 1801
+outSOFUN <- subset(outSOFUN, year>= 1801)
+outSOFUN <- subset(outSOFUN, PFT== 1)
+outBiomeE <- subset(outBiomeE, PFT== 1)
+
+ggplot() +
+  geom_line(data=outBiomeE, aes(x=year, y=GPP), color='blue', size = 2) +
+  geom_line(data=outSOFUN, aes(x=year, y=GPP), color='red') + 
+  xlim( c(0,200) )
+
+ggplot() +
+  geom_line(data=outBiomeE, aes(x=year, y=leafN), color='blue', size = 2) +
+  geom_line(data=outSOFUN, aes(x=year, y=leafN), color='red')
+
+## Loop for columns in two data.frames
+plot_dfs <- lapply(names(outBiomeE),function(nm)data.frame(col1 = outBiomeE[,1],col2 = outBiomeE[,nm], col3 = outSOFUN[,nm]))
+for (idx in seq_along(plot_dfs)){
+  tiff(file = paste(names(outBiomeE)[idx], '.tiff', sep = ''))
+  print(
+    ggplot()+geom_smooth(data = plot_dfs[[idx]], aes(x=col1, y=col2),color="blue") +
+      geom_smooth(data = plot_dfs[[idx]], aes(x=col1, y=col3),color="red")+
+      ggtitle(names(outBiomeE)[idx]))
+  #ggsave("path.tiff")
+  dev.off()
+}
+#for (df in plot_dfs){
+# print(
+#  ggplot()+geom_line(data = df, aes(x=col1, y=col2),color="blue") +
+#           geom_line(data = df, aes(x=col1, y=col3),color="red"))}
